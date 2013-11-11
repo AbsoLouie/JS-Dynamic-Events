f1526e18ad393b6220014b3206fe43b4aa358410
Apprentice <apprentice@dbc-28.local>
2013-11-10 20:22:01 -0800

    Completed

--- application.js
+++ application.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
   var todoTemplate = $.trim($('#todo_template').html());
 
+  // console.log(todoTemplate)
+
   function bindEvents() {
-    // Bind functions which add, remove, and complete todos to the appropriate
-    // elements
+    $('.add').on('click', function() {
+      add()
+    })
+
+    $('.todo_list').on("click", ".complete", function(e) {
+      e.preventDefault
+      $(this).closest(".todo").addClass("complete")
+    })
+
+    $('.todo_list').on("click", ".delete", function(e) {
+      e.preventDefault
+      $(this).closest(".todo").remove()
+    })
+
   }
 
-  //Create functions to add, remove and complete todos
+  var add = function() {
+    var input = $("input").val();
+    var output = buildTodo(input);
+    $('.todo_list').append(output);
+  }
 
-  
+
+  // $('.add').on('click', function() {
+  //   var input = $("input").val();
+  //   var output = buildTodo(input);
+  //   $('.todo_list').append(output);
+  // })
+
+
+
+
+
 
   function buildTodo(todoName) {
     // Creates an jQueryDOMElement from the todoTemplate.
@@ -18,7 +46,6 @@ $(document).ready(function() {
     // Returns the jQueryDOMElement to be used elsewhere.
     return $todo;
   }
-  
 
   bindEvents();
 });
